c4e017dddcd526835f3cc676eed28ada5054fe1e
mikaylarandall <mikaylamrandall@gmail.com>
2024-03-20 09:38:07 -0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using Mission11.Models;
+using Mission11.Models.ViewModels;
+using System.Diagnostics;
+
+namespace Mission11.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IBookRepository _repo;
+        public HomeController(IBookRepository temp)
+        {
+            _repo = temp;
+        }
+
+        public IActionResult Index(int pageNum) // don't ever name pageNum page -- means something to dotnet
+        {
+            int pageSize = 10;
+
+            var bookData = new BookListViewModel
+            {
+                Books = _repo.Books
+                .OrderBy(x => x.Title)
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize),
+
+                PaginationInfo = new PaginationInfo
+                {
+                    CurrentPage = pageNum,
+                    ItemsPerPage = pageSize,
+                    TotalItems = _repo.Books.Count()
+                }
+
+            };
+
+            return View(bookData);
+        }
+
+    }
+}
---
+++ Models/BookstoreContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mission11.Models;
+
+public partial class BookstoreContext : DbContext
+{
+
+    public BookstoreContext(DbContextOptions<BookstoreContext> options) : base(options) { }
+
+    public virtual DbSet<Book> Books { get; set; }
+}
